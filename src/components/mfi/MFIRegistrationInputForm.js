0f9efee471eff7415d3bc47385a6bd674ed6d5cd
ggavarra <gayathri1.g@gmail.com>
2019-08-12 21:17:54 +0800

    Adding axios

--- src/components/mfi/MFIRegistrationInputForm.js
+++ src/components/mfi/MFIRegistrationInputForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Formik } from 'formik';
 import MFIRegistration from './MFIRegistration';
+import axios from 'axios'
 //import Button from "@material-ui/core/Button";
 //import * as Yup from 'yup'
 
@@ -14,7 +15,14 @@ export default class LoginInputForm extends Component {
 
     //invokes rest API
     registerMFI = (values) => {
-        alert('handle submit in parent class ! Hurray' + values.firstName);
+        alert('handle submit in parent class ! Hurray' + JSON.stringify(values));
+        var payload=JSON.stringify(values)
+        axios.post('http://localhost:8081/mfi',payload).then((response) => {
+            console.log(response);
+            })
+        .catch((error) => {
+           alert(error)
+          })
     }
 
     render() {
