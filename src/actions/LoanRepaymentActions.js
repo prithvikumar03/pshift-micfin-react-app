cd184065db0d9d73b50a7d691dfd84a789d7ecac
Prithvi <gayathri1.g@gmail.com>
2019-09-08 16:50:55 +0800

    Just anything

--- src/actions/LoanRepaymentActions.js
+++ src/actions/LoanRepaymentActions.js
@@ -1,11 +1,12 @@
 export const LOAN_REPAYMENT = 'LOAN_REPAYMENT';
 export const LOAN_REPAYMENT_SUCCESS = 'LOAN_REPAYMENT_SUCCESS';
 export const LOAN_REPAYMENT_FAILURE = 'LOAN_REPAYMENT_FAILURE';
-
+/* test */
 export const loanRepaymentAction = (microentrepreneur) => ({
     type: LOAN_REPAYMENT,
     payload: microentrepreneur      
 });
+
 
 export const loanRepaymentSuccess = (microentrepreneur) => ({
     type:  LOAN_REPAYMENT_SUCCESS,
